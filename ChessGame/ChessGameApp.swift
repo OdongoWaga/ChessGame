489331687d52699ace67338b653a56d8b51a936f
Patrick Waga <patrickwaga@icloud.com>
2024-06-08 10:15:53 -0400

    Initial Commit

---
+++ ChessGame/ChessGameApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright iOS Mastery.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
